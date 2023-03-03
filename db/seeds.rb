8a8f268e287a66fc3eef926493d52e653e3c0d41
Leah Perri <perrileah2023@gmail.com>
2023-03-03 13:12:06 -0600

    seeded songs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+songs = Song.create([{ title: "Lonely Boy", album: "El Camino", artist: "The Black Keys", year: 2011 }, { title: "Always Alright", album: "Always Alright", artist: "Alabama Shakes", year: 2013 }, { title: "Heat Waves", album: "Dreamland", artist: "Glass Animals", year: 2020 }])
